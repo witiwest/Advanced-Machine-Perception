602c562f6c881ab0791b2ad545ab05f7a4cebdf5
rvantienen <rvantienen@tudelft.nl>
2025-06-10 12:33:19 +0200

    Update __init__.py

--- model/losses/__init__.py
+++ model/losses/__init__.py
@@ -1,3 +1,5 @@
 from .gaussian_focal_loss import GaussianFocalLoss
 from .l1_loss import L1Loss
 from .losses_utils import reduce_loss, weight_reduce_loss
+from .diou_loss2 import DIoU3DLoss
+from .smooth_l1_loss import SmoothL1Loss
